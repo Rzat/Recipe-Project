b32d95a0abfef05b75f001f49b967f6dbd2bd0c2
rajat <rajatthakur0026@gmail.com>
2019-11-06 22:47:07 +0530

    Adding NumberFormatException error page

--- src/test/java/com/recipeproject/controller/RecipeControllerTest.java
+++ src/test/java/com/recipeproject/controller/RecipeControllerTest.java
@@ -101,5 +101,14 @@ class RecipeControllerTest {
                 .andExpect(view().name("404error"));
     }
 
+    @Test
+    public void testGetRecipeBadRequest() throws Exception {
+     //   when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+        mockMvc.perform(get("/recipe/abc/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+
+    }
+
 
 }
