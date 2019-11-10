9226633b0985cbc293f992b6ea5b7db4c9e93b9d
rajatthakur00@gmail.com <rajat@55>
2019-11-10 14:00:39 +0530

    Adding Controller Advice

--- src/test/java/com/recipeproject/controller/ImageControllerTest.java
+++ src/test/java/com/recipeproject/controller/ImageControllerTest.java
@@ -34,7 +34,9 @@ public class ImageControllerTest {
         MockitoAnnotations.initMocks(this);
 
         imageController = new ImageController(imageService, recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(imageController)
+                .setControllerAdvice(new ControllerExceptionHandler())
+                .build();
     }
 
     @Test
@@ -88,6 +90,13 @@ public class ImageControllerTest {
 
         byte[] responseBytes = response.getContentAsByteArray();
         assertEquals(s.getBytes().length, responseBytes.length);
+    }
+
+    @Test
+    public void tetImageNUmberFormatException() throws Exception {
+        mockMvc.perform(get("/recipe/abc/recipeimage"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 
 }
